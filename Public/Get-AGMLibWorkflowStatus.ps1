0890f35920cdbd5e5ed7865aae86e175ecec4aa5
avandewerdt <>
2020-09-08 10:03:34 +1000

    0.0.0.15

--- Public/Get-AGMLibWorkflowStatus.ps1
+++ Public/Get-AGMLibWorkflowStatus.ps1
@@ -1,5 +1,31 @@
-Function Get-AGMLibWorkflowStatus ([string]$workflowid,[string]$appid,[switch]$refresh)
+Function Get-AGMLibWorkflowStatus ([string]$workflowid,[string]$appid,[switch]$refresh,[switch][alias("p")]$previous)
 {
+    <#
+    .SYNOPSIS
+    Monitors a workflow
+
+    .EXAMPLE
+    Get-AGMLibWorkflowStatus
+    Runs a guided menu to let you select a workflow
+
+    .EXAMPLE
+    Get-AGMLibWorkflowStatus -workflowid 1234
+    Gets the current status of workflow 1234
+
+    .EXAMPLE
+    Get-AGMLibWorkflowStatus -workflowid 1234 -prev
+    Gets the previous status of workflow 1234
+
+    .EXAMPLE
+    Get-AGMLibWorkflowStatus -workflowid 1234 -refresh
+    Gets the current status of workflow 1234 and monitors it till completion
+
+    .DESCRIPTION
+    A function to monitor workflows
+
+    #>
+
+
     # its pointless procededing without a connection.
     if ( (!($AGMSESSIONID)) -or (!($AGMIP)) )
     {
@@ -16,31 +42,46 @@ Function Get-AGMLibWorkflowStatus ([string]$workflowid,[string]$appid,[switch]$r
         }
     }
     $datefields = "startdate,enddate"
-    if ( (!($appid)) -or (!($workflowid)) )
+    if (!($workflowid))
     {
-        $guided = $true
         Clear-Host
         Write-Host "Workflow selection menu"
-        $workflowgrab = Get-AGMWorkFlow  | sort-object name
+        if ($appid)
+        {
+            $workflowgrab = Get-AGMWorkFlow -filtervalue appid=$appid  | sort-object name
+        }
+        else 
+        {
+            $workflowgrab = Get-AGMWorkFlow  | sort-object name
+        }
         if ($workflowgrab.id.count -eq 0)
         {
             Get-AGMErrorMessage -messagetoprint "Failed to find any workflows to list"
             return
         }
         Clear-Host
-        write-host "Workflow selection menu - which Workflow will be run"
+        write-host "Workflow selection menu - which Workflow will be checked"
         Write-host ""
         $i = 1
         foreach ($flow in $workflowgrab)
         { 
-            Write-Host -Object "$i`: Name: $($flow.name)  Workflow ID: $($flow.id)  AppName:  $($flow.application.appname) AppID:  $($flow.application.id)   Appliance: $($flow.cluster.name)  Schedule:   $($flow.schedule.frequency)"
-            $i++
+        $flow | Add-Member -NotePropertyName select -NotePropertyValue $i
+        $flow | Add-Member -NotePropertyName workflowid -NotePropertyValue $flow.id
+        $flow | Add-Member -NotePropertyName appname -NotePropertyValue $flow.application.appname
+        $flow | Add-Member -NotePropertyName appid -NotePropertyValue $flow.application.id
+        $flow | Add-Member -NotePropertyName appliancename -NotePropertyValue $flow.cluster.name
+        $flow | Add-Member -NotePropertyName frequency -NotePropertyValue $flow.schedule.frequency
+        $i++
         }
+        Clear-Host
+        write-host "Workflow selection menu - which Workflow will be run"
+        Write-host ""
+        $workflowgrab | select-object select,name,workflowid,appname,appid,appliancename,frequency | Format-table *
         While ($true) 
         {
             Write-host ""
             $listmax = $workflowgrab.name.count
-            [int]$userselection = Read-Host "Please select an workflow to check (1-$listmax)"
+            [int]$userselection = Read-Host "Please select a workflow to check (1-$listmax)"
             if ($userselection -lt 1 -or $userselection -gt $listmax)
             {
                 Write-Host -Object "Invalid selection. Please enter a number in range [1-$($listmax)]"
@@ -52,23 +93,47 @@ Function Get-AGMLibWorkflowStatus ([string]$workflowid,[string]$appid,[switch]$r
         }
         $appid = $workflowgrab.application.id[($userselection - 1)]
         $workflowid =  $workflowgrab.id[($userselection - 1)]
-        write-host "Command to run is:  Get-AGMLibWorkflowStatus -appid $appid -workflowid $workflowid"
+        if ($prev)
+        {
+            write-host "Command to run is:  Get-AGMLibWorkflowStatus -workflowid $workflowid -prev"
+        }
+        else 
+        {
+            write-host "Command to run is:  Get-AGMLibWorkflowStatus -workflowid $workflowid"
+        }
+
     }
+
+    if ($previous)
+    {
+        $jobgrab = (Get-AGMWorkFlow -filtervalue id=$workflowid).status.prev
+        foreach ($field in $datefields.Split(","))
+        {
+            if ($jobgrab.$field)
+            {
+                $jobgrab.$field = Convert-FromUnixDate $jobgrab.$field
+            }
+        }
+        $jobgrab
+        return
+    }
+
+
 
     if (!($refresh))
     {
-        (Get-AGMApplicationWorkflow  -id $appid | where-object {$_.id -eq $workflowid } | select-object status).status.current
+        (Get-AGMWorkFlow -filtervalue id=$workflowid).status.current
     }
     else 
     {
         $done = 0
         do 
         {
-            $jobgrab = (Get-AGMApplicationWorkflow  -id $appid | where-object {$_.id -eq $workflowid } | select-object status).status.current
+            $jobgrab = (Get-AGMWorkFlow -filtervalue id=$workflowid).status.current
             if ($jobgrab.status -ne "RUNNING")
             {   
                 $done = 1
-                $jobgrab = (Get-AGMApplicationWorkflow  -id $appid | where-object {$_.id -eq $workflowid } | select-object status).status.prev
+                $jobgrab = (Get-AGMWorkFlow -filtervalue id=$workflowid).status.prev
                 # time stamp conversion
                 if ($datefields)
                 {
--- Public/Start-AGMLibWorkflow.ps1
+++ Public/Start-AGMLibWorkflow.ps1
@@ -1,5 +1,30 @@
-Function Start-AGMLibWorkflow ([string]$workflowid,[string]$appid,[switch]$refresh,[switch][alias("g")]$guided)
+Function Start-AGMLibWorkflow ([string]$workflowid,[string]$appid,[switch]$refresh,[switch][alias("m")]$monitor)
 {
+    <#
+    .SYNOPSIS
+    Starts a workflow
+
+    .EXAMPLE
+    Start-AGMLibWorkflow
+    Runs a guided menu to let you start a workflow
+
+    .EXAMPLE
+    Start-AGMLibWorkflow -workflowid 1234
+    Starts workflowflow id 1234 using the latest image
+
+    .EXAMPLE
+    Start-AGMLibWorkflow -workflowid 1234 -refresh
+    Starts workflowflow id 1234 using the latest image refreshing an existing image
+
+    .EXAMPLE
+    Start-AGMLibWorkflow -workflowid 1234 -refresh -monitor
+    Starts workflowflow id 1234 using the latest image refreshing an existing image, then monitors the workflow to completion
+
+    .DESCRIPTION
+    A function to start workflows
+
+    #>
+
     # its pointless procededing without a connection.
     if ( (!($AGMSESSIONID)) -or (!($AGMIP)) )
     {
@@ -15,34 +40,55 @@ Function Start-AGMLibWorkflow ([string]$workflowid,[string]$appid,[switch]$refre
             return
         }
     }
+    if ( (!($appid)) -and ($workflowid) )
+    {   
+        $appid = (Get-AGMWorkFlow -filtervalue id=$workflowid).application.id
+        if (!($appid))
+        {
+            Get-AGMErrorMessage -messagetoprint "Could not determine appid using workflow ID $workflowid."
+            return
+        }
+    }
 
 
     # if we don't get an appid, then lets presume we are going to build a command
     if ( (!($appid)) -or (!($workflowid)) )
     {
-        $guided = $true
         Clear-Host
         Write-Host "Workflow selection menu"
-        $workflowgrab = Get-AGMWorkFlow  | sort-object name
+        if ($appid)
+        {
+            $workflowgrab = Get-AGMWorkFlow -filtervalue appid=$appid  | sort-object name
+        }
+        else 
+        {
+            $workflowgrab = Get-AGMWorkFlow  | sort-object name
+        }
         if ($workflowgrab.id.count -eq 0)
         {
             Get-AGMErrorMessage -messagetoprint "Failed to find any workflows to list"
             return
         }
-        Clear-Host
-        write-host "Workflow selection menu - which Workflow will be run"
-        Write-host ""
         $i = 1
         foreach ($flow in $workflowgrab)
         { 
-            Write-Host -Object "$i`: Name: $($flow.name)  Workflow ID: $($flow.id)  AppName:  $($flow.application.appname) AppID:  $($flow.application.id)   Appliance: $($flow.cluster.name)  Schedule:   $($flow.schedule.frequency)"
-            $i++
+        $flow | Add-Member -NotePropertyName select -NotePropertyValue $i
+        $flow | Add-Member -NotePropertyName workflowid -NotePropertyValue $flow.id
+        $flow | Add-Member -NotePropertyName appname -NotePropertyValue $flow.application.appname
+        $flow | Add-Member -NotePropertyName appid -NotePropertyValue $flow.application.id
+        $flow | Add-Member -NotePropertyName appliancename -NotePropertyValue $flow.cluster.name
+        $flow | Add-Member -NotePropertyName frequency -NotePropertyValue $flow.schedule.frequency
+        $i++
         }
+        Clear-Host
+        write-host "Workflow selection menu - which Workflow will be run"
+        Write-host ""
+        $workflowgrab | select-object select,name,workflowid,appname,appid,appliancename,frequency | Format-table *
         While ($true) 
         {
             Write-host ""
             $listmax = $workflowgrab.name.count
-            [int]$userselection = Read-Host "Please select an workflow to run (1-$listmax)"
+            [int]$userselection = Read-Host "Please select a workflow to run (1-$listmax)"
             if ($userselection -lt 1 -or $userselection -gt $listmax)
             {
                 Write-Host -Object "Invalid selection. Please enter a number in range [1-$($listmax)]"
@@ -123,5 +169,9 @@ Function Start-AGMLibWorkflow ([string]$workflowid,[string]$appid,[switch]$refre
     $json = $body | ConvertTo-Json -depth 10
 
     Post-AGMAPIData  -endpoint /application/$appid/workflow/$workflowid -body $json
+    if ($monitor)
+    {
+        Get-AGMLibWorkflowStatus -workflowid $workflowid -refresh
+    }
 
 }
