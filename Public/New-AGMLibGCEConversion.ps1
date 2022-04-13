377cfe6c496247c626a0a952ff191d7c0173beae
Anthony Vandewerdt <aussiestorblog@gmail.com>
2022-04-13 17:39:12 +1000

    0.0.0.44

--- Public/New-AGMLibGCEConversion.ps1
+++ Public/New-AGMLibGCEConversion.ps1
@@ -151,45 +151,44 @@ Function New-AGMLibGCEConversion([string]$appid,[string]$appname,[string]$imagei
         if ($userselectionapps -eq "" -or $userselectionapps -eq 1)  { $vmgrab = Get-AGMApplication -filtervalue "managed=true&apptype=SystemState&apptype=VMBackup&sourcecluster=$mountapplianceid" | sort-object appname }
         if ($userselectionapps -eq 2) { $vmgrab = Get-AGMApplication -filtervalue "managed=false&apptype=SystemState&apptype=VMBackup&sourcecluster=$mountapplianceid" | sort-object appname  }
         if ($userselectionapps -eq 3) { $vmgrab = Get-AGMApplication -filtervalue "apptype=SystemState&apptype=VMBackup&sourcecluster!$mountapplianceid&clusterid=$mountapplianceid" | sort-object appname }
-        if ($vmgrab.count -eq 0)
+        if ($vmgrab.id.count -eq 0)
         {
             if ($userselectionapps -eq "" -or $userselectionapps -eq 1)  { Get-AGMErrorMessage -messagetoprint "There are no managed System State or VMware apps to list" }
             if ($userselectionapps -eq 2)  { Get-AGMErrorMessage -messagetoprint "There are no unmanaged System State or VMware apps to list" }
             if ($userselectionapps -eq 3)  { Get-AGMErrorMessage -messagetoprint "There are no imported System State or VMware apps to list.  You may need to run Import-AGMLibOnVault first" }
             return
         }
-        if ($vmgrab.count -eq 1)
+        
+        write-host ""
+        write-host "VM Selection menu"
+        Write-host ""
+        $i = 1
+        foreach ($vm in $vmgrab)
+        { 
+            Write-Host -Object "$i`: $($vm.appname) ($($vm.apptype) AppID $($vm.id)) on $($vm.cluster.name)"
+            $i++
+        }
+        While ($true) 
+        {
+            Write-host ""
+            $listmax = $vmgrab.appname.count
+            [int]$vmselection = Read-Host "Please select an application (1-$listmax)"
+            if ($vmselection -lt 1 -or $vmselection -gt $listmax)
+            {
+                Write-Host -Object "Invalid selection. Please enter a number in range [1-$($listmax)]"
+            } 
+            else
+            {
+                break
+            }
+        }
+        if ($vmgrab.id.count -eq 1)
         {
             $appname =  $vmgrab.appname
             $appid = $vmgrab.id
-            write-host "Found one app $appname"
-            write-host ""
         }
-        else
+        else 
         {
-            Clear-Host
-            write-host "VM Selection menu"
-            Write-host ""
-            $i = 1
-            foreach ($vm in $vmgrab)
-            { 
-                Write-Host -Object "$i`: $($vm.appname) ($($vm.apptype) AppID $($vm.id)) on $($vm.cluster.name)"
-                $i++
-            }
-            While ($true) 
-            {
-                Write-host ""
-                $listmax = $vmgrab.appname.count
-                [int]$vmselection = Read-Host "Please select an application (1-$listmax)"
-                if ($vmselection -lt 1 -or $vmselection -gt $listmax)
-                {
-                    Write-Host -Object "Invalid selection. Please enter a number in range [1-$($listmax)]"
-                } 
-                else
-                {
-                    break
-                }
-            }
             $appname =  $vmgrab.appname[($vmselection - 1)]
             $appid = $vmgrab.id[($vmselection - 1)]
         }
