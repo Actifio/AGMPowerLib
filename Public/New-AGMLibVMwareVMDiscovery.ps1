b02c15f413618868b841df0e91e57a6d62672911
Jing Qin <qjing@google.com>
2023-08-29 13:00:54 -0400

    add missing prameter into examples

--- Public/New-AGMLibVMwareVMDiscovery.ps1
+++ Public/New-AGMLibVMwareVMDiscovery.ps1
@@ -40,28 +40,28 @@ If the following are specified in combination then all instances will have a bac
 -backup -sltid <slt ID learned with Get-AGMSLT> -slpid <slp ID learned with Get-AGMSLP>
 
 .EXAMPLE
-New-AGMLibVMwareVMDiscovery -discoveryfile discovery.csv -username user-01@abc.com
+New-AGMLibVMwareVMDiscovery -vmtag mytag -discoveryfile discovery.csv -username user-01@abc.com
 
 .EXAMPLE
-New-AGMLibVMwareVMDiscovery -discoveryfile discovery.csv -nobackup -username user-01@abc.com
+New-AGMLibVMwareVMDiscovery -vmtag mytag -discoveryfile discovery.csv -nobackup -username user-01@abc.com
 
 .EXAMPLE
-New-AGMLibVMwareVMDiscovery -discoveryfile discovery.csv -sltid 2121511 -slpid 6006 -username user-01@abc.com -passfilepath '.vcenterpass'
+New-AGMLibVMwareVMDiscovery -vmtag mytag -discoveryfile discovery.csv -sltid 2121511 -slpid 6006 -username user-01@abc.com -passfilepath '.vcenterpass'
 
 .EXAMPLE
-New-AGMLibVMwareVMDiscovery -discoveryfile discovery.csv -sltname LocalSnap -slpname LocalProfile -username user-01@abc.com -passfilepath '.vcenterpass'
+New-AGMLibVMwareVMDiscovery -vmtag mytag -discoveryfile discovery.csv -sltname LocalSnap -slpname LocalProfile -username user-01@abc.com -passfilepath '.vcenterpass'
 
 .EXAMPLE
-New-AGMLibVMwareVMDiscovery -applianceid 143112195179 -vcenterid 2110151 -username user-01@abc.com -passfilepath '.vcenterpass'
+New-AGMLibVMwareVMDiscovery -vmtag mytag -applianceid 143112195179 -vcenterid 2110151 -username user-01@abc.com -passfilepath '.vcenterpass'
 
 .EXAMPLE
-New-AGMLibVMwareVMDiscovery -applianceid 143112195179 -vcenterid 2110151 -nobackup -username user-01@abc.com -passfilepath '.vcenterpass'
+New-AGMLibVMwareVMDiscovery -vmtag mytag -applianceid 143112195179 -vcenterid 2110151 -nobackup -username user-01@abc.com -passfilepath '.vcenterpass'
 
 .EXAMPLE
-New-AGMLibVMwareVMDiscovery -applianceid 143112195179 -vcenterid 2110151 -sltid 2121511 -slpid 6006 -username user-01@abc.com -passfilepath '.vcenterpass'
+New-AGMLibVMwareVMDiscovery -vmtag mytag -applianceid 143112195179 -vcenterid 2110151 -sltid 2121511 -slpid 6006 -username user-01@abc.com -passfilepath '.vcenterpass'
 
 .EXAMPLE
-New-AGMLibVMwareVMDiscovery -applianceid 143112195179 -vcenterid 2110151 -sltname LocalSnap -slpname LocalProfile -username user-01@abc.com -passfilepath '.vcenterpass'
+New-AGMLibVMwareVMDiscovery -vmtag mytag -applianceid 143112195179 -vcenterid 2110151 -sltname LocalSnap -slpname LocalProfile -username user-01@abc.com -passfilepath '.vcenterpass'
 #>
 function New-AGMLibVMwareVMDiscovery {
     [CmdletBinding()]
